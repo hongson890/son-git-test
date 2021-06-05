e4a877287cf5703bec1911d9186784f410680fee
Son.Pham <hongson890@gmail.com>
2021-06-05 16:34:26 +0700

    fixing bug

--- src/test-utils/mongo/MongooseTestModule.ts
+++ src/test-utils/mongo/MongooseTestModule.ts
@@ -1,5 +1,5 @@
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server-core';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 let mongod: MongoMemoryServer;
 
